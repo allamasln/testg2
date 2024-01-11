1124202f4b9d0469f02aacc6c363f1b8fca6d53e
Antonio Llamas <antoniollamasln@gmail.com>
2024-01-11 16:43:57 +0100

    fix icons

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,6 @@
+const styles = require('@fortawesome/fontawesome-svg-core/styles.css')
+const config = require('@fortawesome/fontawesome-svg-core')
+
+config.autoAddCss = false
+
 import './src/styles/global.css'
